ed78ebc4eec74e9402a43e3015577fee5c0b8976
Belange Jan <belangejan@gmail.com>
2026-07-22 23:13:52 +0200

    Connexion à la base AlwaysData

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$serveur = "localhost";
-$utilisateur = "root";
-$motDePasse = "";
-$baseDeDonnees = "mon_panier";
+$serveur = "mysql-jnbelange.alwaysdata.net";
+$utilisateur = "jnbelange";
+$motDePasse = "Gtmbobine@2026";
+$baseDeDonnees = "jnbelange_mon_panier";
 
 $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
 
@@ -11,4 +11,4 @@ if ($connexion->connect_error) {
     die("Erreur de connexion : " . $connexion->connect_error);
 }
 
-?>
+$connexion->set_charset("utf8mb4");
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1d6a3a2832d2c237d70a7ff5fd79b48b71e38b3d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1d6a3a2832d2c237d70a7ff5fd79b48b71e38b3d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
